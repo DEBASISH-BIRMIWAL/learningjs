3abd1379f89cebf342d0401a6f0ac964bab7a952
deb <94240802+DEBASISH-BIRMIWAL@users.noreply.github.com>
2024-06-23 17:57:00 +0000

    learnt conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valInNumber = Number("true")
+console.log(valInNumber)
+console.log(typeof (valInNumber))
+
+/*
+"69" => 69
+"69o0" => NaN
+true/false => 0/1
+"true/false" => NaN
+undefined => NaN
+null => NaN
+
+*/
